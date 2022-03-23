e8b555ec46efa55024a1949be48ad06b1c8d000e
kesselaprod <robert.grolms@gmail.com>
2022-03-23 09:29:00 +0100

    renaming, debugging

--- pip_install_dep.cmd
+++ pip_install_dep.cmd
@@ -4,5 +4,6 @@ call .\env\Scripts\activate.bat
 .\env\Scripts\python.exe -m pip list
 .\env\Scripts\python.exe -m pip install --upgrade pip
 .\env\Scripts\python.exe -m pip install -r requirements.txt
+.\env\Scripts\python.exe -m pip list
 ::.\env\Scripts\python.exe -m pip freeze > requirements.txt
 :: https://stackoverflow.com/questions/14684968/how-to-export-virtualenv
